e70ff4ad96c748e5e35bdbf872cd9782953e4e58
hzhu <zhuhao6590@outlook.com>
2025-05-08 15:44:46 +0800

    docs - update readme
    fix
    - fix import error due to spinful fermion

--- quante/generate/operas/__init__.py
+++ quante/generate/operas/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # @Author: hzhu
 # @Date:   2024-12-15 19:14:46
-# @Last Modified by:   dzwang
-# @Last Modified time: 2025-02-26 16:17:51
+# @Last Modified by:   hzhu
+# @Last Modified time: 2025-05-08 13:15:10
 
 
 from .spin import *
 
 from .fermion import FermionOper, SpinfulFermionOper
-from .fermion import FermionBuilder, SpinfullFermionBuilder
+from .fermion import FermionBuilder, SpinfulFermionBuilder
 
 from .boson import BosonOper
