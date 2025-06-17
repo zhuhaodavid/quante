aaa6504c8f60abea562d65d0d92a5d5efa4381b6
hzhu <zhuhao6590@outlook.com>
2025-06-17 15:10:36 +0800

    style - rename EvolveEngine as time_indp_evolve

--- quante/linalg/evolve/__init__.py
+++ quante/linalg/evolve/__init__.py
@@ -2,10 +2,10 @@
 # @Author: hzhu
 # @Date:   2025-06-16 18:38:03
 # @Last Modified by:   hzhu
-# @Last Modified time: 2025-06-17 10:32:38
+# @Last Modified time: 2025-06-17 15:05:36
 
 from .EDevolve import *
 from .expm_multiply import *
-from .EvolveEngine import *
+from .time_indp_evolve import *
 from .super_operator import *
 from .chebyshev import *
