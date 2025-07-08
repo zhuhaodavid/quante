966febc07475684a3e424bc9de8749584ebe3db2
hzhu <zhuhao6590@outlook.com>
2025-07-08 16:54:22 +0800

    docs

--- tests/bridge/__init__.py
+++ tests/bridge/__init__.py
@@ -2,9 +2,9 @@
 # @Author: hzhu
 # @Date:   2025-07-07 11:55:08
 # @Last Modified by:   hzhu
-# @Last Modified time: 2025-07-07 11:55:52
+# @Last Modified time: 2025-07-08 16:54:14
 
+# this file load all the bridge files
 
 from .tenpy_utils import *
 from .torch_utils import *
-
