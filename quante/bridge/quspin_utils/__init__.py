b43c0592008a2d3934f234c859bd51270c85c140
hzhu <zhuhao6590@outlook.com>
2025-07-23 00:42:08 +0800

    feat - add clean_static in quspintools

--- quante/bridge/quspin_utils/__init__.py
+++ quante/bridge/quspin_utils/__init__.py
@@ -2,11 +2,11 @@
 # @Author: hzhu
 # @Date:   2025-07-07 12:03:27
 # @Last Modified by:   hzhu
-# @Last Modified time: 2025-07-22 15:25:39
+# @Last Modified time: 2025-07-23 00:41:30
 
 
 from .gen_matrix import optimize_basis
-
+from oper_mani import clean_static, clean_static2
 
 
 
